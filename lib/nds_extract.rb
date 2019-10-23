81dd7e0adc5ca2d43f1df88de761bc0e3ada8a7c
Jerry Huang <jhuang9240@gmail.com>
2019-10-23 01:06:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,5 +25,5 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  return 1357566430
+  hash = directors_totals(director_data)
 end
